19819de69a64459fad167b662e508e38e058603d
ADITYA JAIN <aditya.jain.210899@gmail.com>
2021-08-04 23:16:30 +0530

    Add files via upload

---
+++ start.c
@@ -0,0 +1,20 @@
+
+#include <gtk/gtk.h>
+
+int main (int argc, char **argv) {
+
+gtk_init (&argc, &argv);
+
+GtkWidget *window;
+
+window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+
+g_signal_connect (window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+gtk_widget_show_all(window);
+
+gtk_main ();
+
+return 0;
+}
+
